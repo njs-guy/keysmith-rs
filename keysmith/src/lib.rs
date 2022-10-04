5f7d3eb25c51ee1fda90b90cd7ad33cd13c0c58d
Nick Shockey <shockeynj@gmail.com>
2022-10-04 14:37:24 -0400

    can actually generate a key now

--- keysmith/src/lib.rs
+++ keysmith/src/lib.rs
@@ -12,6 +12,13 @@ fn rand_digit() -> u8 {
 
 // Generates a key string
 pub fn gen_key(length: u32) -> String {
+    // Gen options. Will be optional args later.
+    let nums = true;
+    let letters = true;
+    let s_sp_ch = true;
+    let u_sp_ch = false;
+
+    // Usable characters
     let eng_alphabet = "abcdefghijklmnopqrstuvwxyz";
     let safe_sp_chars = "-_.()~@";
     let unsafe_sp_chars = r#"#%&*+={}\/<>?!$:'"`|"#;
@@ -19,13 +26,44 @@ pub fn gen_key(length: u32) -> String {
     // See this https://stackoverflow.com/a/40415059
     // Might want to look more into this later
 
+    let mut chars = String::from("");
+
+    if nums {
+        chars.push('0');
+    }
+
+    if letters {
+        chars.push_str(eng_alphabet);
+    }
+
+    if s_sp_ch {
+        chars.push_str(safe_sp_chars);
+    }
+
+    if u_sp_ch {
+        chars.push_str(unsafe_sp_chars);
+    }
+
     let mut output = String::from("");
 
-    output.push_str(eng_alphabet);
-    output.push_str(safe_sp_chars);
-    output.push_str(unsafe_sp_chars);
-    let x = rand_digit().to_string();
-    output.push_str(&x);
+    for _n in 1..=length {
+        let mut rng = rand::thread_rng();
+        let idx = rng.gen_range(0..chars.len());
+        let c = chars.chars().nth(idx).unwrap();
+
+        let result:String;
+
+        match c {
+            '0' => {
+                result = rand_digit().to_string();
+            },
+            _=> {
+                result = c.to_string();
+            }
+        }
+
+        output.push_str(&result);
+    }
 
     output // Return output as String
 }
