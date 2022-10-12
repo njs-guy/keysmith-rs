bd4ce78f481cd5b40dad7c6dc07fba7633085165
Nick Shockey <shockeynj@gmail.com>
2022-10-12 15:38:33 -0400

    Can now generate version 4 UUIDs

--- keysmith/src/lib.rs
+++ keysmith/src/lib.rs
@@ -1,10 +1,10 @@
 mod gen_char;
 use gen_char::gen_char;
 
-// Generates a key string
+// Generates a key string. Ex: LlyqZk2W-Hm6Eoid~m(A8ymiM0q3ksyJ
 pub fn gen_key(length: u32) -> String {
     let mut output = String::from("");
-    for _n in 1..length {
+    for _n in 1..=length {
         let c = gen_char(
             true, // nums
             true, // letters
@@ -17,6 +17,42 @@ pub fn gen_key(length: u32) -> String {
     }
 
     output // Return output as String
+}
+
+// Generate numbers and letters (no uppercase)
+fn gen_uuid_v4_char() -> char {
+    gen_char(true, true, false, false, false)
+}
+
+// Generate a uuid (version 4). Ex: e54h7tmn-b8kp-hykv-kn0o-nl9aypd9kc9e
+pub fn gen_uuid_v4() -> String {
+    let mut output = String::from("");
+
+    for _n in 1..=8 {
+        let c = gen_uuid_v4_char();
+        output.push(c);
+    }
+
+    output.push('-'); // Current state: e54h7tmn-
+
+    for _n in 1..=3 {
+        for _x in 1..=4 {
+            let c = gen_uuid_v4_char();
+            output.push(c);
+        }
+
+        output.push('-');
+        // After one loop: e54h7tmn-b8kp-
+    }
+
+    // e54h7tmn-b8kp-hykv-kn0o-
+
+    for _n in 1..=12 {
+        let c = gen_uuid_v4_char();
+        output.push(c);
+    }
+
+    output // e54h7tmn-b8kp-hykv-kn0o-nl9aypd9kc9e
 }
 
 #[cfg(test)]
--- ks-tests/src/main.rs
+++ ks-tests/src/main.rs
@@ -1,8 +1,18 @@
 use keysmith;
 
 fn main() {
+    println!("Keys:");
+    println!("");
     for _n in 1..=10 {
-        let key: String = keysmith::gen_key(64);
+        let key: String = keysmith::gen_key(32);
+        println!("{}", key);
+    }
+
+    println!("");
+    println!("UUIDs (version 4):");
+    println!("");
+    for _n in 1..=10 {
+        let key: String = keysmith::gen_uuid_v4();
         println!("{}", key);
     }
 }
