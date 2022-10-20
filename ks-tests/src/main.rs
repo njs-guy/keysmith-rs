0fee75bc1104a27c8e5694d9e284bc8cadfb16e2
Nick Shockey <shockeynj@gmail.com>
2022-10-20 17:30:39 -0400

    gen_nums() lets you generate a numbers only key

--- ks-tests/src/main.rs
+++ ks-tests/src/main.rs
@@ -3,26 +3,46 @@ use keysmith;
 fn main() {
     let num_of_keys = 10;
 
-    println!("Keys:");
-    println!("");
-    for _n in 1..=num_of_keys {
-        let key: String = keysmith::gen_key(32);
-        println!("{}", key);
+    let keys = false;
+    let nums = true;
+    let uuid4 = false;
+    let uuidn = false;
+
+    if keys {
+        println!("Keys:");
+        println!("");
+        for _n in 1..=num_of_keys {
+            let key: String = keysmith::gen_key(32);
+            println!("{}", key);
+        }
     }
 
-    println!("");
-    println!("UUIDs (v4):");
-    println!("");
-    for _n in 1..=num_of_keys {
-        let key: String = keysmith::gen_uuid('4');
-        println!("{}", key);
+    if nums {
+        println!("Numbers only:");
+        println!("");
+        for _n in 1..=num_of_keys {
+            let key: String = keysmith::gen_nums(32);
+            println!("{}", key);
+        }
     }
 
-    println!("");
-    println!("UUIDs (nonstandard):");
-    println!("");
-    for _n in 1..=num_of_keys {
-        let key: String = keysmith::gen_uuid('n');
-        println!("{}", key);
+    if uuid4 {
+        println!("");
+        println!("UUIDs (v4):");
+        println!("");
+        for _n in 1..=num_of_keys {
+            let key: String = keysmith::gen_uuid('4');
+            println!("{}", key);
+        }
+    }
+
+    if uuidn {
+        println!("");
+        println!("UUIDs (nonstandard):");
+        println!("");
+        for _n in 1..=num_of_keys {
+            let key: String = keysmith::gen_uuid('n');
+            println!("{}", key);
+        }
     }
 }
