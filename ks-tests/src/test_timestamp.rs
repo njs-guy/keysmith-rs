f239e00ef8e2254808bcf07a89df8416d707c1ca
Nick Shockey <shockeynj@gmail.com>
2022-11-04 12:28:48 -0400

    Prettier formatting on the rest of the test crate

--- ks-tests/src/test_timestamp.rs
+++ ks-tests/src/test_timestamp.rs
@@ -1,23 +1,22 @@
 use keysmith::timestamp;
 
 pub struct TestTimestampOpts {
-    pub string: bool,
-    pub i64: bool,
+	pub string: bool,
+	pub i64: bool,
 }
 
 pub fn test_timestamp(opts: TestTimestampOpts) {
+	if opts.string {
+		println!("Timestamp as String:");
+		println!("");
+		let stamp: String = timestamp::get_timestamp();
+		println!("{}", stamp);
+	}
 
-    if opts.string {
-        println!("Timestamp as String:");
-        println!("");
-        let stamp: String = timestamp::get_timestamp();
-        println!("{}", stamp);
-    }
-
-    if opts.i64 {
-        println!("Timestamp as i64:");
-        println!("");
-        let stamp: i64 = timestamp::get_timestamp_i64();
-        println!("{}", stamp);
-    }
+	if opts.i64 {
+		println!("Timestamp as i64:");
+		println!("");
+		let stamp: i64 = timestamp::get_timestamp_i64();
+		println!("{}", stamp);
+	}
 }
--- ks-tests/src/test_uuid.rs
+++ ks-tests/src/test_uuid.rs
@@ -1,28 +1,28 @@
 use keysmith::uuid;
 
 pub struct TestUuidOpts {
-    pub uuid4: bool,
-    pub uuidn: bool,
+	pub uuid4: bool,
+	pub uuidn: bool,
 }
 
 pub fn test_uuid(num_of_keys: u32, opts: TestUuidOpts) {
-    if opts.uuid4 {
-        println!("");
-        println!("UUIDs (v4):");
-        println!("");
-        for _n in 1..=num_of_keys {
-            let key: String = uuid::gen_uuid('4');
-            println!("{}", key);
-        }
-    }
+	if opts.uuid4 {
+		println!("");
+		println!("UUIDs (v4):");
+		println!("");
+		for _n in 1..=num_of_keys {
+			let key: String = uuid::gen_uuid('4');
+			println!("{}", key);
+		}
+	}
 
-    if opts.uuidn {
-        println!("");
-        println!("UUIDs (nonstandard):");
-        println!("");
-        for _n in 1..=num_of_keys {
-            let key: String = uuid::gen_uuid('n');
-            println!("{}", key);
-        }
-    }
+	if opts.uuidn {
+		println!("");
+		println!("UUIDs (nonstandard):");
+		println!("");
+		for _n in 1..=num_of_keys {
+			let key: String = uuid::gen_uuid('n');
+			println!("{}", key);
+		}
+	}
 }
