d5df57e0f58db8baaf1d3221ad0f3523e29b1a24
Nick Shockey <shockeynj@gmail.com>
2022-11-06 12:06:50 -0500

    Can now internally gen a timestamp from any epoch

--- keysmith/src/timestamp.rs
+++ keysmith/src/timestamp.rs
@@ -2,10 +2,12 @@
 
 use chrono::{ DateTime, Utc };
 
+const DEF_EPOCH: &str = "Sat, 1 Jan 2022 00:00:00 +0000";
+
 // Generates a timestamp from the seconds since 00:00:00 Jan 1, 2022
-fn gen_timestamp() -> i64 {
+fn gen_timestamp(epoch_str: &str) -> i64 {
 	let epoch = DateTime::parse_from_rfc2822(
-		"Sat, 1 Jan 2022 00:00:00 +0000"
+		epoch_str
 	).expect("ERROR: Could not get timestamp epoch.");
 
 	let utc = Utc::now();
@@ -16,14 +18,14 @@ fn gen_timestamp() -> i64 {
 
 /// Gets a timestamp from the seconds since 00:00:00 Jan 1, 2022 as a String
 pub fn get_timestamp() -> String {
-	let stamp = gen_timestamp().to_string();
+	let stamp = gen_timestamp(DEF_EPOCH).to_string();
 
 	stamp
 }
 
 /// Gets a timestamp from the seconds since 00:00:00 Jan 1, 2022 as an i64
 pub fn get_timestamp_i64() -> i64 {
-	gen_timestamp()
+	gen_timestamp(DEF_EPOCH)
 }
 
 // TODO: More tests to make sure that the format is correct. Need to allow custom epochs first.
